2eae285de5c4f456e7bc7a1eb42c389db2972f4e
EJain-Dev <ekansh.jain2011@gmail.com>
2025-08-02 18:39:53 -0400

    Added a constructor for the other Pointer test.

--- tests/pointer.cpp
+++ tests/pointer.cpp
@@ -2,6 +2,10 @@
 #include <malloc.h>
 #include <SimpleCPP/Pointer.h>
 
+#include <algorithm>
+#include <random>#include <ranges>
+#include <vector>
+
 size_t allocated_count;
 size_t allocated_size;
 
@@ -31,4 +35,21 @@ TEST_F(PointerTest, LenConstructor) {
   }
   EXPECT_EQ(allocated_count, 0);
   EXPECT_EQ(allocated_size, sizeof(simplecpp::Pointer<float>) * NUM_ELEMENTS);
+}
+
+TEST_F(PointerTest, ExistingDataConstructor) {
+  using type = float;
+  constexpr auto NUM_ELEMENTS = 32;
+  {
+    std::mt19937 gen{NUM_ELEMENTS};
+    std::normal_distribution<type> dist{};
+    std::vector<type> data(NUM_ELEMENTS);
+    std::ranges::generate(data, [&gen, &dist]() { return dist(gen); });
+    simplecpp::Pointer<type, alloc, dealloc> ptr{data.data(), NUM_ELEMENTS};
+    for (size_t i = 0; i < NUM_ELEMENTS; ++i) {
+      EXPECT_EQ(ptr[i], data[i]);
+    }
+  }
+  EXPECT_EQ(allocated_count, 0);
+  EXPECT_EQ(allocated_size, sizeof(type) * NUM_ELEMENTS);
 }
